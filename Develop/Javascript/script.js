c3e5fd0a93560ac3f7ca8ce84ba7470678cbaacf
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-14 12:48:03 -0500

    created function to save text to array

--- Develop/Javascript/script.js
+++ Develop/Javascript/script.js
@@ -4,5 +4,16 @@ $(function(){
     dateElement.text(moment().format('dddd MMMM Do'))
 })
 
-//empty tasks variable to store tasks
-var tasks = {}
+//empty array to store tasks
+var tasks = [];
+
+//function to save text after user as entered
+$(".sv-btn").click(function(){
+    console.log("david degea")
+    //get form values
+    var taskText = $("#9amTasks").val();
+
+    //save in tasks array
+    tasks.push(taskText)
+
+})
