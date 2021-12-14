14bc56bb2769d40612fe1f3266361f8d9a8adf5d
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-14 17:31:20 -0500

    latest code

--- Develop/Javascript/script.js
+++ Develop/Javascript/script.js
@@ -4,24 +4,45 @@ $(function(){
     dateElement.text(moment().format('dddd MMMM Do'))
 })
 
-//empty array to store tasks
+//empty object to store tasks
 var tasks = [];
+
+//function to load tasks from local storage
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    //if nothing in local storage create new array
+    if (!tasks) {
+        tasks = []
+    };
+
+
+
+}
 
 //function that saves task array to local storage
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    console.log("another save by degea")
-}
+    
+};
 
 //function to save text after user as entered
 $(".sv-btn").click(function(){
-    console.log("david degea save button")
+    
     //get form values
     var taskText = $(".form-control").val();
+    
+    //var taskTime = "9";
+    //var taskTime = $(this).next(".task-time").val();
 
     //save in tasks array
-    tasks.push(taskText)
+    tasks.push({
+        text: taskText,
+        //time: taskTime
+    });
 
     saveTasks();
 
 });
+
+//load tasks for the first time
+loadTasks();
