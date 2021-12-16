10762ac37b691d2428facb21598d50cc2e66f5e8
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-16 12:29:22 -0500

    changed save tasks function

--- Develop/Javascript/script.js
+++ Develop/Javascript/script.js
@@ -8,29 +8,28 @@ $(function(){
 var tasks = [];
 
 //function to load tasks from local storage
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    //if nothing in local storage create new array
-    if (!tasks) {
-        tasks = []
-    };
+// var loadTasks = function() {
+//      tasks = JSON.parse(localStorage.getItem("tasks"));
+//      //if nothing in local storage create new array
+//         if (!tasks) {
+//          tasks = []
+//         };
 
-    //select all text forms
-   var allTasks = document.querySelectorAll(".form-control");
+//     //select all text forms
+//    var allTasks = document.querySelectorAll(".form-control");
 
-    allTasks.forEach(a => {
-        for (var i = 0; i < tasks.length; i++) {
-            allTasks[a].value == tasks[i]
-        }
-    })
+//     allTasks.forEach((i) => {
+//         console.log($(this))
+//     })
 
 
-}
+// }
 
 //function that saves task array to local storage
 var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    
+    for (var i = 0; i < tasks.length; i++) {
+    localStorage.setItem(JSON.stringify(tasks[i].id), JSON.stringify(tasks[i].text));
+    }
 };
 
 //function to save text after user as entered
@@ -41,8 +40,18 @@ $(".sv-btn").click(function(){
 
     //loop through each text form 
     for (var i = 0; i < allTasks.length; i++) {
-        console.log(allTasks[i].value);
-        tasks.push(allTasks[i].value);
+        
+        var taskId = allTasks[i].id;
+        var taskText = allTasks[i].value; 
+
+        console.log(allTasks[i].id);
+
+        tasks.push({
+            id: taskId,
+            text: taskText
+        })
+       
+        //tasks.push(allTasks[i].value);
     }
 
     //get form values
@@ -55,5 +64,9 @@ $(".sv-btn").click(function(){
 
 });
 
+$("#clear-storage").click(function(){
+    localStorage.clear();
+    console.log("storage cleared")
+});
 //load tasks for the first time
-loadTasks();
+//loadTasks();
